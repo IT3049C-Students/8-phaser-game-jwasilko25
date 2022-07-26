f63a5d965d7e4c460db147e83c25ebf2b0e230a4
jwasilko25 <105300237+jwasilko25@users.noreply.github.com>
2022-07-26 17:37:04 -0400

    updated game.js

--- game.js
+++ game.js
@@ -15,7 +15,6 @@ var snake;
 var food;
 var cursors;
 
-//  Direction consts
 var UP = 0;
 var DOWN = 1;
 var LEFT = 2;
@@ -25,8 +24,8 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('food', 'assets/games/snake/food.png');
-    this.load.image('body', 'assets/games/snake/body.png');
+    this.load.image('food', 'assets/spritesheets/food.png');
+    this.load.image('body', 'assets/spritesheets/body.png');
 }
 
 function create ()
@@ -152,11 +151,7 @@ function create ()
 
             this.direction = this.heading;
 
-            //  Update the body segments and place the last coordinate into this.tail
             Phaser.Actions.ShiftPosition(this.body.getChildren(), this.headPosition.x * 16, this.headPosition.y * 16, 1, this.tail);
-
-            //  Check to see if any of the body pieces have the same x/y as the head
-            //  If they do, the head ran into the body
 
             var hitBody = Phaser.Actions.GetFirst(this.body.getChildren(), { x: this.head.x, y: this.head.y }, 1);
 
@@ -170,7 +165,6 @@ function create ()
             }
             else
             {
-                //  Update the timer ready for the next movement
                 this.moveTime = time + this.speed;
 
                 return true;
@@ -192,7 +186,6 @@ function create ()
 
                 food.eat();
 
-                //  For every 5 items of food eaten we'll increase the snake speed a little
                 if (this.speed > 20 && food.total % 5 === 0)
                 {
                     this.speed -= 5;
@@ -208,7 +201,6 @@ function create ()
 
         updateGrid: function (grid)
         {
-            //  Remove all body pieces from valid positions list
             this.body.children.each(function (segment) {
 
                 var bx = segment.x / 16;
@@ -238,13 +230,6 @@ function update (time, delta)
         return;
     }
 
-    /**
-    * Check which key is pressed, and then change the direction the snake
-    * is heading based on that. The checks ensure you don't double-back
-    * on yourself, for example if you're moving to the right and you press
-    * the LEFT cursor, it ignores it, because the only valid directions you
-    * can move in at that time is up and down.
-    */
     if (cursors.left.isDown)
     {
         snake.faceLeft();
@@ -264,7 +249,6 @@ function update (time, delta)
 
     if (snake.update(time))
     {
-        //  If the snake updated, we need to check for collision against food
 
         if (snake.collideWithFood(food))
         {
@@ -273,22 +257,10 @@ function update (time, delta)
     }
 }
 
-/**
-* We can place the food anywhere in our 40x30 grid
-* *except* on-top of the snake, so we need
-* to filter those out of the possible food locations.
-* If there aren't any locations left, they've won!
-*
-* @method repositionFood
-* @return {boolean} true if the food was placed, otherwise false
-*/
+
 function repositionFood ()
 {
-    //  First create an array that assumes all positions
-    //  are valid for the new piece of food
-
-    //  A Grid we'll use to reposition the food each time it's eaten
-    var testGrid = [];
+     var testGrid = [];
 
     for (var y = 0; y < 30; y++)
     {
@@ -302,7 +274,6 @@ function repositionFood ()
 
     snake.updateGrid(testGrid);
 
-    //  Purge out false positions
     var validLocations = [];
 
     for (var y = 0; y < 30; y++)
@@ -311,7 +282,6 @@ function repositionFood ()
         {
             if (testGrid[y][x] === true)
             {
-                //  Is this position valid for food? If so, add it here ...
                 validLocations.push({ x: x, y: y });
             }
         }
@@ -319,10 +289,8 @@ function repositionFood ()
 
     if (validLocations.length > 0)
     {
-        //  Use the RNG to pick a random food position
         var pos = Phaser.Math.RND.pick(validLocations);
 
-        //  And place it
         food.setPosition(pos.x * 16, pos.y * 16);
 
         return true;
